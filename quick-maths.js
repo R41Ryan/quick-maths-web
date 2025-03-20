d090fc8108faa2f9a9401dcd849628a3673297c8
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2025-03-20 00:07:42 -0600

    added a numpad for mobile support

--- quick-maths.js
+++ quick-maths.js
@@ -1,5 +1,6 @@
 var operationChoice = document.querySelector("#operation-choice");
 var gameInterface = document.querySelector("#game-interface");
+var answerElement = document.querySelector(".answer");
 
 var operand1;
 var operand2;
@@ -14,7 +15,7 @@ var inPlay = false;
 function setQuestionText() {
   document.querySelector("#question").innerText =
     operand1 + " " + operation + " " + operand2;
-}
+};
 
 function createQuestion() {
   operand1 = Math.floor(Math.random() * 50);
@@ -36,11 +37,11 @@ function createQuestion() {
       break;
     default:
   }
-}
+};
 
 function checkAnswer() {
   var answerElement = document.querySelector(".answer");
-  if (answerElement.innerHTML == correctAnswer) {
+  if (answerElement.innerText == correctAnswer) {
     score++;
     setScoreText();
     answerElement.classList.add("correct");
@@ -48,33 +49,32 @@ function checkAnswer() {
       answerElement.classList.remove("correct");
       createQuestion();
       setQuestionText();
-      answerElement.innerHTML = "";
+      answerElement.innerText = "";
     }, 250);
   }
-}
+};
 
 function setScoreText() {
-  document.querySelector("#score").innerHTML = "Score: " + score;
-}
+  document.querySelector("#score").innerText = "Score: " + score;
+};
 
 function setTimerText() {
   var timeElapsed = Date.now() - startTime;
   var minutes = Math.floor(timeElapsed / 1000 / 60);
   var seconds = Math.floor(timeElapsed / 1000 - minutes * 60);
-  document.querySelector("#timer").innerHTML =
+  document.querySelector("#timer").innerText =
     String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
-}
+};
 
 document.querySelector("html").addEventListener("keydown", function (event) {
   console.log(event.key);
-  var answerElement = document.querySelector(".answer");
   if (inPlay && !answerElement.classList.contains("correct")) {
     if (event.key >= "0" && event.key <= "9") {
       answerElement.innerText += event.key;
     } else if (event.key == "Backspace" || event.key == "Delete") {
-      answerElement.innerHTML = answerElement.innerHTML.slice(0, -1);
-    } else if (event.key == "-" && answerElement.innerHTML.length == 0) {
-      answerElement.innerHTML += "-";
+      answerElement.innerText = answerElement.innerText.slice(0, -1);
+    } else if (event.key == "-" && answerElement.innerText.length == 0) {
+      answerElement.innerText += "-";
     }
 
     checkAnswer();
@@ -84,7 +84,7 @@ document.querySelector("html").addEventListener("keydown", function (event) {
 var operationButtons = document.querySelectorAll("#operation-choice button");
 for (let i = 0; i < operationButtons.length; i++) {
   operationButtons[i].addEventListener("click", function () {
-    operation = operationButtons[i].innerHTML;
+    operation = operationButtons[i].innerText;
     inPlay = true;
     score = 0;
     operationChoice.remove();
@@ -96,7 +96,7 @@ for (let i = 0; i < operationButtons.length; i++) {
     setQuestionText();
     setScoreText();
   });
-}
+};
 
 var mainMenuButtons = document.querySelectorAll(".back-to-main-menu-btn");
 for (let i = 0; i < mainMenuButtons.length; i++) {
@@ -106,6 +106,24 @@ for (let i = 0; i < mainMenuButtons.length; i++) {
     document.querySelector("main").appendChild(operationChoice);
     clearInterval(timerIntervalId);
   });
-}
+};
+
+var numpadButtons = document.querySelectorAll(".num");
+for (let i = 0; i < numpadButtons.length; i++) {
+  numpadButtons[i].addEventListener("click", function () {
+    if (!answerElement.classList.contains("correct")) {
+      answerElement.innerText += numpadButtons[i].innerText;
+    }
+    checkAnswer();
+  });
+};
+document.querySelector(".minus").addEventListener("click", function () {
+  if (answerElement.innerText.length == 0) {
+    answerElement.innerText = "-";
+  }
+});
+document.querySelector(".delete").addEventListener("click", function () {
+  answerElement.innerText = answerElement.innerText.slice(0, -1);
+});
 
 gameInterface.remove();
