1466e6692225586dff0f701b60b754fab7c11361
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2025-04-29 21:48:17 -0600

    no scores available msg span 4 cols

--- quick-maths-app/src/PersonalProgressChart.jsx
+++ quick-maths-app/src/PersonalProgressChart.jsx
@@ -1,3 +1,4 @@
+import "./PersonalProgressChart.css"
 import {
   LineChart,
   Line,
@@ -19,7 +20,7 @@ function PersonalProgressChart({ setScreen }) {
   const [data, setData] = useState([]);
 
   const isMobile = useMediaQuery({
-    query: '(max-width: 768px)'
+    query: "(max-width: 768px)",
   });
   const chartWidth = isMobile ? 300 : 600;
 
@@ -47,43 +48,45 @@ function PersonalProgressChart({ setScreen }) {
   return (
     <div id="personal-progress-chart">
       <h2>Personal Progress Chart</h2>
-      <div className="chart-container">
-        <div className="chart-inner">
-          <LineChart
-            width={chartWidth}
-            height={300}
-            data={data}
-            margin={{ top: 5, right: 10, left: 10, bottom: 20 }}
-          >
-            <CartesianGrid stroke="#444" height={300} />
-            <XAxis dataKey="date" stroke="#8884d8">
-              <Label
-                value="Date"
-                offset={-10}
-                position="insideBottom"
-                fill="#8884d8"
+      {scores.length <= 0 ? (
+        <h2>No Scores Available</h2>
+      ) : (
+        <div className="chart-container">
+          <div className="chart-inner">
+            <LineChart
+              width={chartWidth}
+              height={300}
+              data={data}
+              margin={{ top: 5, right: 10, left: 10, bottom: 20 }}
+            >
+              <CartesianGrid stroke="#444" height={300} />
+              <XAxis dataKey="date" stroke="#8884d8">
+                <Label
+                  value="Date"
+                  offset={-10}
+                  position="insideBottom"
+                  fill="#8884d8"
+                />
+              </XAxis>
+              <YAxis stroke="#8884d8">
+                <Label
+                  value="Score"
+                  offset={-10}
+                  position="insideLeft"
+                  fill="#8884d8"
+                />
+              </YAxis>
+              <Line
+                type="monotone"
+                dataKey="score"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
               />
-            </XAxis>
-            <YAxis stroke="#8884d8">
-              <Label
-                value="Score"
-                offset={-10}
-                position="insideLeft"
-                fill="#8884d8"
-              />
-            </YAxis>
-            <Line
-              type="monotone"
-              dataKey="score"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-            />
-          </LineChart>
-          <button onClick={() => setScreen("mainMenu")}>
-            Back to Main Menu
-          </button>
+            </LineChart>
+          </div>
         </div>
-      </div>
+      )}
+      <button className="back-to-main-menu-btn" onClick={() => setScreen("mainMenu")}>Back to Main Menu</button>
     </div>
   );
 }
